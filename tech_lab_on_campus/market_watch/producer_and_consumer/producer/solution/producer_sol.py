f0d79e6b04cb7162c618a2334f5bd60a8894d6f0
Aadvika <aadvikaxpandey@gmail.com>
2026-02-11 02:37:06 +0000

    feat:implemented producer functionality

--- tech_lab_on_campus/market_watch/producer_and_consumer/producer/solution/producer_sol.py
+++ tech_lab_on_campus/market_watch/producer_and_consumer/producer/solution/producer_sol.py
@@ -1,3 +1,4 @@
+
 # Copyright 2024 Bloomberg Finance L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,22 +12,35 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import pika
-class mqProducerInterface:
+import os
+from producer_interface import mqProducerInterface
+class mqProducer(mqProducerInterface):
     def __init__(self, routing_key: str, exchange_name: str) -> None:
         # Save parameters to class variables
+        self.routing_key=routing_key
+        self.exchange_name=exchange_name
         # Call setupRMQConnection
-        pass
+        self.setupRMQConnection()
 
     def setupRMQConnection(self) -> None:
         # Set-up Connection to RabbitMQ service  
+        #self.connection = pika.BlockingConnection(
+        #pika.ConnectionParameters(host='localhost'))
+        con_params = pika.URLParameters(os.environ["AMQP_URL"])
+        self.connection = pika.BlockingConnection(parameters=con_params) 
         # Establish Channel
+        self.channel = self.connection.channel()
         # Create the exchange if not already present
+        self.exchange = self.channel.exchange_declare(exchange=self.exchange_name)
         pass
 
     def publishOrder(self, message: str) -> None:
-         # Basic Publish to Exchange
+        # Basic Publish to Exchange
+        self.channel.basic_publish(exchange=self.exchange_name,routing_key=self.routing_key,body=message,
+)
         # Close Channel
+        self.channel.close()
         # Close Connection
+        self.connection.close()
         pass
